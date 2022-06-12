a6541e98d56549659b2b184e0efd4010ba03872d
hig4342 <hig4342@naver.com>
2022-06-12 13:42:34 +0900

    Ssukzip SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+
+const colors = {
+  kakao: {
+    500: '#fee500'
+  }
+}
+
+const theme = extendTheme({ colors })
+const MUITheme = createTheme({
+
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={MUITheme}>
+      <ChakraProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+      </ChakraProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
